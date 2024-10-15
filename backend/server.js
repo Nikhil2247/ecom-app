0dc44d4eb3b27ff745fd5cd13e3c404b036825e2
Nikhil Kumar <153814768+Nikhil2247@users.noreply.github.com>
2024-10-15 10:31:46 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '10mb' }));
 
 // Serve static files from the 'uploads' folder
-app.use('/uploads', express.static('/uploads'));
+app.use('/uploads', express.static('../uploads'));
 
 // Routes
 app.use('/api/auth', authRoutes);
